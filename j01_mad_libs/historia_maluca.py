68b61a1992cc7789abaf116de84c0fb1041697fd
Rebeca <rebeca.magri@edu.senai.br>
2026-05-04 14:58:54 -0300

    ADD: JogoTeca

---
+++ j01_mad_libs/historia_maluca.py
@@ -0,0 +1,28 @@
+def jogar_mad_libs():
+
+  print("""
+    ▄█    █▄     ▄█     ▄████████     ███      ▄██████▄     ▄████████  ▄█     ▄████████        ▄▄▄▄███▄▄▄▄      ▄████████  ▄█       ███    █▄   ▄████████    ▄████████ 
+    ███    ███   ███    ███    ███ ▀█████████▄ ███    ███   ███    ███ ███    ███    ███      ▄██▀▀▀███▀▀▀██▄   ███    ███ ███       ███    ███ ███    ███   ███    ███ 
+    ███    ███   ███▌   ███    █▀     ▀███▀▀██ ███    ███   ███    ███ ███▌   ███    ███      ███   ███   ███   ███    ███ ███       ███    ███ ███    █▀    ███    ███ 
+  ▄███▄▄▄▄███▄▄ ███▌   ███            ███   ▀ ███    ███  ▄███▄▄▄▄██▀ ███▌   ███    ███      ███   ███   ███   ███    ███ ███       ███    ███ ███          ███    ███ 
+  ▀▀███▀▀▀▀███▀  ███▌ ▀███████████     ███     ███    ███ ▀▀███▀▀▀▀▀   ███▌ ▀███████████      ███   ███   ███ ▀███████████ ███       ███    ███ ███        ▀███████████ 
+    ███    ███   ███           ███     ███     ███    ███ ▀███████████ ███    ███    ███      ███   ███   ███   ███    ███ ███       ███    ███ ███    █▄    ███    ███ 
+    ███    ███   ███     ▄█    ███     ███     ███    ███   ███    ███ ███    ███    ███      ███   ███   ███   ███    ███ ███▌    ▄ ███    ███ ███    ███   ███    ███ 
+    ███    █▀    █▀    ▄████████▀     ▄████▀    ▀██████▀    ███    ███ █▀     ███    █▀        ▀█   ███   █▀    ███    █▀  █████▄▄██ ████████▀  ████████▀    ███    █▀  
+                                                            ███    ███                                                     ▀                                            
+        """)
+
+  verbo= input("Qual o verbo escolhido?")
+  animal= input("Qual o animal escolhido?")
+  adj= input("Qual o adjetivo escolhido?")
+  obj= input("Qual o objeto escolhido?")
+  lugar= input("Qual o lugar escolhido?")
+  emoção= input("Qual a emoção escolhida?")
+  print(f"""
+  Era uma vez um(a) {animal} que gostava de {verbo} um {obj}. 
+  Certo dia ele procurou seu/sua {obj} e não encontrou, então ele foi até a sala de estar e telefonou para sua amiga abelha que era muito/a {adj}. 
+  Então ela disse a ele/a que, ele/a deveria comprar outro/a {obj}, até por que aquele {obj} já havia se perdido pela cidade, 
+  sendo assim sua amiga abelha muito {adj} o/a aconcelhou a buscar em outra cidade, por que assim, as chances dele/a encontrar um {obj} identico ao seu eram grandes.
+  Sabendo disso o/a {animal} se mudou para o/a {lugar}, lugar em que existiam muitos {obj} como aquele que o/a {animal} havia perdido.
+  Enfim, o/a {animal} e seu/a {obj} sentiram muito/a {emoção}.
+  """)
---
+++ main.py
@@ -0,0 +1,71 @@
+from j02_adivinhe_numero.adivinha_numero import jogar_adivinha_numero
+from j01_mad_libs.historia_maluca import jogar_mad_libs
+from j03_tabuada.tabuada import jogar_tabuada
+from j04_cara_or_coroa.cara_or_coroa import jogar_cara_coroa
+from j05_impar_or_par.game_impar_or_par import jogar_impar_par
+from j06_jogo_das_tres_coisas.jogo_das_tres_coisas import jogar_jogo_tres_coisas
+
+while True:
+
+    print("""
+        
+         ▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄     ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ 
+        █   █       █       █       █   █       █       █       █      █
+        █   █   ▄   █   ▄▄▄▄█   ▄   █   █▄     ▄█    ▄▄▄█       █  ▄   █
+    ▄   █   █  █ █  █  █  ▄▄█  █ █  █     █   █ █   █▄▄▄█     ▄▄█ █▄█  █
+    █ █▄█   █  █▄█  █  █ █  █  █▄█  █    █   █ █    ▄▄▄█    █  █      █
+    █       █       █  █▄▄█ █       █    █   █ █   █▄▄▄█    █▄▄█  ▄   █
+    █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█    █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█ █▄▄█
+        
+       .--..--..--..--..--..--.
+      .' \  (`._   (_)     _   \
+     .'    |  '._)         (_)  |
+     \ _.')\      .----..---.   /
+     |(_.'  |    /    .-\-.  \  |
+     \     0|    |   ( O| O) | o|
+      |  _  |  .--.____.'._.-.  |
+       \ (_) | o         -` .-`  |
+        |    \   |`-._ _ _ _ _\ /
+        \    |   |  `. |_||_|   |
+        | o  |    \_      \     |     -.   .-.
+        |.-.  \     `--..-'   O |     `.`-' .'
+    _.'  .' |     `-.-'      /-.__   ' .-'
+    .' `-.` '.|='=.='=.='=.='=|._/_ `-'.'
+    `-._  `.  |________/\_____|    `-.'
+    .'   ).| '=' '='\/ '=' |
+    `._.`  '---------------'
+            //___\   //___\
+                ||       ||
+                ||_.-.   ||_.-.
+                (_.--__) (_.--__)
+        
+    ############################################################
+    ##                                                        ##
+    ##                  0 - SAIR                              ##
+    ##                  1 - Mad Libs                          ##
+    ##                  2 - Adivinhe o número                 ##
+    ##                  3 - Tabuada                           ##
+    ##                  4 - Cara ou Coroa                     ##
+    ##                  5 - Impar ou Par                      ##
+    ##                  6 - Jogo das três coisas              ##
+    ##                                                        ##
+    ############################################################
+        """)
+
+
+    escolha = int(input("Com qual jogo iremos nos divertir?"))
+    if escolha == 1:
+        jogar_mad_libs()
+    elif escolha == 2:
+        jogar_adivinha_numero()
+    elif escolha == 3:
+        jogar_tabuada()
+    elif escolha == 4:
+        jogar_cara_coroa()
+    elif escolha == 5:
+        jogar_impar_par()
+    elif escolha == 6:
+        jogar_jogo_tres_coisas()
+    elif escolha == 0:
+        print("Foi bom jogar com você!")
+        break
