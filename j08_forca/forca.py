8e2b4503434d3200ddde166affbb16eaba600e56
Rebeca <rebeca.magri@edu.senai.br>
2026-05-19 11:26:47 -0300

    ADD:FORCA

--- j08_forca/forca.py
+++ j08_forca/forca.py
@@ -7,7 +7,7 @@ def limpar_tela():
 
 def escolher_palavra() -> str:
     """Escolhe e retorna uma palavra aleatória"""
-    palavras = ["OI", "CASA", "GODOFREDO", "PAPEL", "URACILA", "CARBONO", "MEMÓRIA", "INDECENTE", "DESENVOLVER", "GARFO","CITOSINA","MONTANHOSA","BRAVO"]
+    palavras = ["OI", "CASA", "GODOFREDO", "PAPEL", "URACILA", "CARBONO", "MEMORIA", "INDECENTE", "DESENVOLVER", "GARFO","CITOSINA","MONTANHOSA","BRAVO", "ALMOCAR","AMOR","CLOROFILA","PASTEL"]
     palavra_aleatoria = random.choice(palavras)
     #Retorna a palavra para quem chamou a função
     #Assim que ele "return" a função, ele não pode mais fazer nada, nem printar.
@@ -108,11 +108,10 @@ def gerar_tracos(palavra:str) -> list:
 
 def perguntar_letra():
     """Pergunto uma letra"""
-    perguntar_letra = input("Qual a letra? ").upper()
-    if perguntar_letra != 1:
-        print('Eu disse UMA letra seu noob!')
-        input("Qual a letra? ").upper()
-    return perguntar_letra
+    letra = input("Qual a letra? ").upper()
+    while len(letra) != 1:
+        letra = input('Eu disse UMA letra seu noob!Qual a letra? ').upper()
+    return letra 
 
 def jogar_forca():
         print("""
@@ -120,10 +119,69 @@ def jogar_forca():
         |    | ____   ____   ____     __| _/____    \_   _____/__________   ____ _____   
         |    |/  _ \ / ___\ /  _ \   / __ |\__  \    |    __)/  _ \_  __ \_/ ___  __  \  
     /\__|    (  <_> ) /_/  >  <_> ) / /_/ | / __ \_  |     \(  <_> )  | \/\  \___ / __ \_
-    \________|\____/\___  / \____/  \____ |(____  /  \___  / \____/|__|    \___  >____  /
-                /_____/               \/     \/       \/                    \/     \/ 
-
+    \________|\____/\___  / \____/  \____ |(____  /  \___  / \____/|__|    \___  >____  /                         
+                                                                
+                                  ....            
+                                ..........        
+                              ......  ......      
+                              ................    
+                            ....................  
+                          ....................    
+                        ......................  ..
+                        ....................  ..  
+                      ....................        
+                    ......................  ..    
+                  ......................  ..      
+                  ....................            
+                ......................  ..        
+              ++....................  ..          
+            ++++++................  ..            
+            ++++++++  ..........  ..              
+          ++++++        ......    ..              
+        ++++++            ....  ..                
+      ++++++++                ..                  
+      ++mm++                                      
+    ++++++                                        
+   ++++++                                          
+  ++MM++                                          
+  ++++                                            
+              
             """)
     
+        input("Pressione ENTER para jogar...")
+        contador_erro = 0
+        tentativas = []
+
+        e_p=escolher_palavra()
+        g_t=gerar_tracos(e_p)
+    
+        while True:
+            limpar_tela()
+
+            desenhar_forca(contador_erro)
+            print("                 ", *g_t)
+            print(*tentativas)
+
+            if "_" not in g_t:
+              print("Que perfeição!!!")
+              print("Voce merece um Parabuains!")
+              break
+            p_l=perguntar_letra()
+
+            if p_l not in e_p:
+                contador_erro += 1
+                tentativas.append(p_l)
+                if contador_erro == 8:
+                    print("Voce perdeuu!!!")
+                    print(f"A palavra era {e_p}")
+                    break
+                
+            if p_l in e_p:
+                contador = 0
+                for letra_palavra in e_p:
+                    if letra_palavra == p_l:
+                        g_t [contador] = p_l
+                    contador += 1
+
 if __name__ == "__main__":
     jogar_forca()
